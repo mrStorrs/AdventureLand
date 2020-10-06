96714d7b2dfda77207ffb108abf338d6770a22aa
mrStorrs <cj.storrs@gmail.com>
2020-10-06 14:13:06 -0700

    added selling func, and rarity func

--- Functions/Upgrade.js
+++ Functions/Upgrade.js
@@ -1,46 +1,69 @@
-//show_json(character.items);
+//run compound script @ same time as upgrade. 
+load_code("Compound");
 
 //enter the items and desired level here, and rarity
 // 1 = store bought. 2 = common. 3 =rare.
 // 'nameOfItem' : [*level*, *rarity*]
 
 var upgrade_item = {
-  'cclaw': [3, 1],
+						'wshoes' : [6, 1],
+						'wcap' : [6, 1],
+						'cclaw' : [5, 1],
+						'wgloves' : [5, 1],
+						'wbreeches' : [5, 1],
+						'mushroomstaff' : [5, 1],
+
+					}
+//dict so i can use in operator. maybe use 1 for something later
+var sell_item = {"stinger" : 0, "hpamulet" : 0, "hpbelt" : 0}
+
+//method to upgrade items
+function upgrade_items(item_index, cur_lev, rarity) {
+	if (distance(character, find_npc("newupgrade")) > 300){
+		smart_move(find_npc("newupgrade")); //move to upgrade loc
+	} else { //upgrade
+		//check if rarity and level matches
+		if(cur_lev > 4 && rarity == 1){
+			upgrade(item_index, 1);
+		} else {
+			upgrade(item_index, 0);
+		}
+		
+	}
+	return null;
 }
-var last_upgrade = new Date().getTime() // used to stop upgrading spam
 
-//this will be the logic for the upgrading
-
-function upgrade_items(item_index, rarity) {
-  if (distance(character, find_npc("newupgrade")) > 50) {
-    smart_move(find_npc("newupgrade")); //move to upgrade loc
-  } else { //upgrade
-    last_upgrade = new Date().getTime();
-    upgrade(item_index, 0);
-  }
-  return null;
-}
-
-setInterval(function () {
-  var time = new Date().getTime();
-  items = character.items //set items (easier to type)
-  //if upgrading move to cue
-
-
-  //check if any items in inventory need upgrading
-  //starts at 1 because I keep scrolls in 1. change as needed.
-  for (i = 1; i < items.length; i++) {
-
-    if (items[i] != null) { //check if item present in inv slot.
-      //check if needs to be upgraded.
-      if (items[i].name in upgrade_item &&
-        upgrade_item[items[i].name][0] > items[i].level) {
-        set_message("upgrading");
-        upgrade_items(i, 0); //upgrade
-        break; //end  when item found
-
-      }
+setInterval(function(){
+	items = character.items //set items (easier to type)
+	
+	//check if need basic scrolls
+	if(character.items[0].q < 10)
+	{
+		buy('scroll0', 100);
     }
-  }
-}, 6000); // Loops every 6 seconds.. Avoids upgrade spam.
+    
+	//check if any items in inventory need upgrading
+	//starts at 1 because I keep scrolls from 0-1. change as needed.
+	for( i = 1; i < items.length; i++){
+		
+		if (items[i] != null){ //check if item present in inv slot.
+			//check if needs to be upgraded.
+			if (items[i].name in upgrade_item &&
+					//this may need to be refactored look into later
+					//may have a problem with more than 1 item in list
+			  		upgrade_item[items[i].name][0] > items[i].level)
+			{ 
+				set_message("upgrading");
+				upgrade_items(i, items[i].level,
+							 upgrade_item[items[i].name][1] ); //upgrade
+				
+				break; //end  when item found
+			} else if (items[i].name in sell_item
+					   && sell_item[items[i].name] == items[i].level)
+			{
+				sell(i, 1);		   
+			}
+		}
+	}
+}, 5000); // Loops every 6 seconds.. Avoids upgrade spam.
 
