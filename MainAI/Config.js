879fbee3277ee174ecd586b9983e677214e4f3de
mrStorrs <cj.storrs@gmail.com>
2020-10-13 20:22:45 -0700

    config alpha

---
+++ MainAI/Config.js
@@ -0,0 +1,35 @@
+/*
+    This is a work in progress. It will become the main script that all characters run.
+        -Everything will be state depended (moving, attacking, upgrading, etc..)
+        -Will have config for all the 4 characters you can run at one time
+        -This will include options for choosing Main Tank / Pulling
+        -Will have skill sets depending on what class the character is at
+        -This particular file will not run an interval. The code blocks that the
+            character loads will be what contains the intervals.
+*/
+/////////////////////////////////////////////////////
+///////Enter your character names here.
+////////////////////////////////////////////////////
+figher_1    = "MoyaTesh";
+fighter_2  = "Boomybob";
+fighter_3   = "SliceNdice";
+merchant    = "NickyNickel";
+
+/////////////////////////////////////////////////////
+///////Choose Character Options here////////////////
+////////////////////////////////////////////////////
+
+//fighter_1/////////////////////////////////////////
+fighter_1.is_tank = false;
+fighter_1.will_kite = false;
+
+//fighter_2/////////////////////////////////////////
+fighter_2.is_tank = false;
+fighter_2.will_kite = false;
+
+//fighter_3/////////////////////////////////////////
+fighter_3.is_tank= false;
+fighter_2.will_kite = false;
+
+
+//merchant//////////////////////////////////////////
