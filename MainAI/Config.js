4b3d31d169b15f2b302e7d412a5b6e7e4266e510
mrStorrs <cj.storrs@gmail.com>
2020-10-18 05:50:17 -0700

    Adding descriptions

--- MainAI/Config.js
+++ MainAI/Config.js
@@ -6,7 +6,7 @@
  * individual scripts.
  * 
  * When completed:
- *  + Everything will be state depended (moving, attacking, upgrading, etc..)
+ *  + Everything will be state dependent(moving, attacking, upgrading, etc..)
  *  + Will have config for all the 4 characters you can run at one time
  *  + This will include options for choosing Main Tank / Pulling
  *  + Will have skill sets depending on what class the character is
@@ -15,12 +15,21 @@
  * --------------------------------------------------------------*/
 
 //Fighter objects
-function Fighter(name){
+function Fighter(name) {
     this.name = name;
+    //will the character attempt to kite the mob.
     this.is_kiteing = false;
-    this.is_pvp = false; 
+    /* determines what the character will do in a pvp situation. Options:
+     *   "aggresive" = will attack others on sight.
+     *   "scared"    = will switch server on sight.
+     *   "bravish"   = will switch server when attacked. */
+    this.pvp =
+    //scripts to load.
     this.activities = ["Fight", "SendItems"]
+    //this will set the fighter to try to grab aggro or pull targets.
     this.is_tank = false;
+    //mobs to farm go here. order is priority.
+    this.mobs_to_farm = ["arcticbee"]        
 }
 
 //array creating and holding all fighter objects.
@@ -40,26 +49,32 @@ all_fighters = [
 //fighter_2 --  "Boomybob"
 
 //fighter_3 --  "SliceNdice"
+fighter_3.activities = [
+    "TESTING",
+    "goldmeter",
+    "SendItems"
+]
 
 /* ----------------------------------------------------------------
  * Configure your merchant here.
  * --------------------------------------------------------------*/
 merchant = {
-    name : "NickyNickel",
-    activities : ["Upgrade", "Compound", "FetchItems", "LuckBuff"]
+    name: "NickyNickel",
+    activities: ["Upgrade", "Compound", "FetchItems", "LuckBuff"]
 }
 
 /* ----------------------------------------------------------------
  * Logic for fighters deciding which scripts to load goes here.
  * --------------------------------------------------------------*/
 //find the currently loaded fighter
-for(fighter of all_fighters){
-    if (character.name == all_fighters[i].name){
-        fighter = all_fighters[i];
+for (current_fighter of all_fighters) {
+    if (character.name == current_fighter.name) {
+        fighter = current_fighter;
+        game_log("Fighter:" + fighter.name + " is ready for battle!")
     }
 }
 //load scripts
-for(activity of fighter.activities){
+for (activity of fighter.activities) {
     load_code(activity);
 }
 
